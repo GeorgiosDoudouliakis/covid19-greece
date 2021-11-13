621d7f45bfaa367093947af8e4879a27a3e290d2
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-13 17:42:13 +0200

    style: Format app routing module code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,16 @@ import { NavigationComponent } from '@shared/components/navigation/navigation.co
 const routes: Routes = [
   { 
     path: '', component: NavigationComponent, children: [
-      { path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), pathMatch: 'full' },
-      { path: 'global-news', loadChildren: () => import('./modules/global-news/global-news.module').then(m => m.GlobalNewsModule) }
+      { path: '', 
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), 
+        pathMatch: 'full' 
+      },
+      { path: 'global-news', 
+        loadChildren: () => import('./modules/global-news/global-news.module').then(m => m.GlobalNewsModule) 
+      },
+      { path: 'regions', 
+        loadChildren: () => import('./modules/regions/regions.module').then(m => m.RegionsModule) 
+      }
     ]
   }
 ];
