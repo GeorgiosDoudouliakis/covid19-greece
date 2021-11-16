d6fb3b089a0ae959b287bfbcbf70a4630eb7add4
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-16 23:02:11 +0200

    feat: Add layout of regions page

---
+++ src/app/modules/regions/components/map/map.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { defaults as defaultInteractions, DragRotateAndZoom } from 'ol/interaction';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import Map from 'ol/Map';
+import * as olProj from 'ol/proj';
+import { fromLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import Text from 'ol/style/Text';
+import View from 'ol/View';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MapComponent implements OnInit {
+  map!: Map;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.map = new Map({
+      target: 'map-container',
+      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([23.727539, 37.983810]),
+        zoom: 6
+      })
+    });
+  }
+}
--- src/app/modules/regions/regions.module.ts
+++ src/app/modules/regions/regions.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RegionsComponent } from './pages/regions/regions.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
     imports: [RouterModule.forChild([{ path: '', component: RegionsComponent}]),SharedModule],
     exports: [RouterModule],
-    declarations: [RegionsComponent],
+    declarations: [RegionsComponent, MapComponent],
     providers: [],
 })
 export class RegionsModule { }
