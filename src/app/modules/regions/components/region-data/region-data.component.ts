3964897a5226e33c1fd48a120f516e7700ed7be0
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-21 19:25:11 +0200

    chore: Add ngx capture package

--- src/app/modules/regions/components/region-data/region-data.component.ts
+++ src/app/modules/regions/components/region-data/region-data.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Region } from '../../models/region.model';
+import { NgxCaptureService } from 'ngx-capture';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-region-data',
@@ -8,10 +11,32 @@ import { Region } from '../../models/region.model';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegionDataComponent implements OnInit {
+  private elementRef!: ElementRef;
+  private destroy$ = new Subject();
   @Input() regionData = {} as Region;
   @Output() regionDataHandler = new EventEmitter();
 
-  constructor() { }
+  constructor(private elRef: ElementRef, private captureService: NgxCaptureService) {
+    this.elementRef = elRef;
+   }
 
   ngOnInit(): void {}
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  takeScreenshot() {
+    const element = document.createElement('a');
+    this.captureService.getImage(this.elementRef.nativeElement, true)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(img => {
+      element.href = img;
+      element.setAttribute("download", "region_data.png");
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    })
+  }
 }
--- src/app/modules/regions/regions.module.ts
+++ src/app/modules/regions/regions.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NgxCaptureModule } from 'ngx-capture';
 import { RegionsComponent } from './pages/regions/regions.component';
 import { MapComponent } from './components/map/map.component';
 import { RegionDataComponent } from './components/region-data/region-data.component';
 
 @NgModule({
-    imports: [RouterModule.forChild([{ path: '', component: RegionsComponent}]),SharedModule],
+    imports: [
+        RouterModule.forChild([{ path: '', component: RegionsComponent}]), 
+        SharedModule, 
+        NgxCaptureModule
+    ],
     exports: [RouterModule],
     declarations: [RegionsComponent, MapComponent, RegionDataComponent],
     providers: [],
