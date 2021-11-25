9a7cfba7b49f26f81a9933485734f212805a5c4a
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-25 22:58:57 +0200

    feat: Main structure of global news page

--- src/app/modules/global-news/global-news.module.ts
+++ src/app/modules/global-news/global-news.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { GlobalNewsComponent } from './pages/global-news/global-news.component';
+import { CountrySelectorComponent } from './components/country-selector/country-selector.component';
+import { NewsComponent } from './components/news/news.component';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
     imports: [RouterModule.forChild([{ path: '', component: GlobalNewsComponent}]),SharedModule],
     exports: [RouterModule],
     declarations: [
-        GlobalNewsComponent
+        GlobalNewsComponent,
+        CountrySelectorComponent,
+        NewsComponent,
+        PaginatorComponent
     ],
     providers: [],
 })
