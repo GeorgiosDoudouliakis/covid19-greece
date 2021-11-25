0a97a7e17351547c48aeccc83f94e30c54394673
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-25 22:44:41 +0200

    style: Improve folder structure

--- src/app/modules/home/components/overall-stats/card/card.component.ts
+++ src/app/modules/home/components/overall-stats/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { OverallStats } from '../../models/overall-stats.model';
+import { OverallStats } from '../../../models/overall-stats.model';
 
 @Component({
   selector: 'app-card',
