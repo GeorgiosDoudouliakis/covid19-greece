66fce87e5e448e5d443d3e6c13f22a30490c8e1f
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-27 14:57:25 +0200

    refactor: Move article component in shared module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,7 +3,6 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ChartsModule } from 'ng2-charts';
 import { AgeDistributionComponent } from './components/age-distribution/age-distribution.component';
-import { ArticleComponent } from './components/latest-covid-news/article/article.component';
 import { CardComponent } from './components/overall-stats/card/card.component';
 import { GenderComponent } from './components/gender/gender.component';
 import { LatestCovidNewsComponent } from './components/latest-covid-news/latest-covid-news.component';
@@ -19,7 +18,6 @@ import { ReplaceCommasWithDotsPipe } from './pipes/replace-commas-with-dots.pipe
     exports: [RouterModule],
     declarations: [
         AgeDistributionComponent, 
-        ArticleComponent, 
         CardComponent, 
         GenderComponent,
         LatestCovidNewsComponent,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,12 +6,27 @@ import { HeaderComponent } from './components/navigation/header/header.component
 import { FooterComponent } from './components/navigation/footer/footer.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { LanguageSelectorComponent } from './components/navigation/header/language-selector/language-selector.component';
+import { ArticleComponent } from './components/article/article.component';
 import { ArrowVisibilityDirective } from './directives/arrow-visibility/arrow-visibility.directive';
 
 @NgModule({
     imports: [CommonModule, RouterModule],
-    exports: [CommonModule, NavigationComponent, LoaderComponent, ArrowVisibilityDirective],
-    declarations: [HeaderComponent, FooterComponent, NavigationComponent, LoaderComponent, LanguageSelectorComponent, ArrowVisibilityDirective],
+    exports: [
+        CommonModule, 
+        NavigationComponent, 
+        LoaderComponent, 
+        ArticleComponent, 
+        ArrowVisibilityDirective
+    ],
+    declarations: [
+        HeaderComponent, 
+        FooterComponent, 
+        NavigationComponent, 
+        LoaderComponent, 
+        LanguageSelectorComponent, 
+        ArticleComponent, 
+        ArrowVisibilityDirective
+    ],
     providers: [],
 })
 export class SharedModule { }
