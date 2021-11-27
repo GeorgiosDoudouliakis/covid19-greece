e748823d568aea521ee91bcada4e3aaacb1641c6
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-27 20:18:28 +0200

    refactor: Add mock data for countries with name and code for each country and use it for the select country dropdown

--- src/app/modules/global-news/components/country-selector/country-selector.component.ts
+++ src/app/modules/global-news/components/country-selector/country-selector.component.ts
@@ -3,6 +3,12 @@ import { FormControl } from '@angular/forms';
 import { CovidNewsService } from '@shared/services/covid-news/covid-news.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { COUNTRIES } from '../../mock/countries.mock';
+
+interface Country {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-country-selector',
@@ -11,13 +17,15 @@ import { takeUntil } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CountrySelectorComponent implements OnInit, OnDestroy {
-  countries: string[] = ['gr', 'ae', 'ar', 'at', 'au', 'be', 'bg', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'];
+  countries: Country[];
   countryControl: FormControl = new FormControl(null);
   private destroy$ = new Subject();
 
   constructor(private covidNewsService: CovidNewsService) { }
 
   ngOnInit(): void {
+    this.countries = COUNTRIES;
+
     this.countryControl.valueChanges
       .pipe(takeUntil(this.destroy$))
       .subscribe(country => this.covidNewsService.countryAction(country));
---
+++ src/app/modules/global-news/mock/countries.mock.ts
@@ -0,0 +1,206 @@
+export const COUNTRIES = [
+    {
+        name: 'Ελλάδα',
+        code: 'gr'
+    },
+    {
+        name: 'Ηνωμένα Αραβικά Εμιράτα',
+        code: 'ae'
+    },
+    {
+        name: 'Αργεντινή',
+        code: 'ar'
+    },
+    {
+        name: 'Αυστρία',
+        code: 'at'
+    },
+    {
+        name: 'Αυστραλία',
+        code: 'au'
+    },
+    {
+        name: 'Βέλγιο',
+        code: 'be'
+    },
+    {
+        name: 'Βουλγαρία',
+        code: 'bg'
+    },
+    {
+        name: 'Καναδάς',
+        code: 'ca'
+    }, 
+    {
+        name: 'Ελβετία',
+        code: 'ch'
+    },
+    {
+        name: 'Κίνα',
+        code: 'cn'
+    },
+    {
+        name: 'Κολομβία',
+        code: 'co'
+    },
+    {
+        name: 'Κούβα',
+        code: 'cu'
+    },
+    {
+        name: 'Τσεχία',
+        code: 'cz'
+    },
+    {
+        name: 'Γερμανία',
+        code: 'de'
+    },
+    {
+        name: 'Αίγυπτος',
+        code: 'eg'
+    },
+    {
+        name: 'Γαλλία',
+        code: 'fr'
+    },
+    {
+        name: 'Ηνωμένο Βασίλειο',
+        code: 'gb'
+    },
+    {
+        name: 'Χονγκ Κονγκ',
+        code: 'hk'
+    },
+    {
+        name: 'Ουγγαρία',
+        code: 'hu'
+    },
+    {
+        name: 'Ινδονησία',
+        code: 'id'
+    },
+    {
+        name: 'Ιρλανδία',
+        code: 'ie'
+    },
+    {
+        name: 'Ισραήλ',
+        code: 'il'
+    },
+    {
+        name: 'Ιταλία',
+        code: 'it'
+    },
+    {
+        name: 'Ιαπωνία',
+        code: 'jp'
+    },
+    {
+        name: 'Νότια Κορέα',
+        code: 'kr'
+    },
+    {
+        name: 'Λιθουανία',
+        code: 'lt'
+    },
+    {
+        name: 'Λετονία',
+        code: 'lv'
+    },
+    {
+        name: 'Μαρόκο',
+        code: 'ma'
+    },
+    {
+        name: 'Μεξικό',
+        code: 'mx'
+    },
+    {
+        name: 'Μαλαισία',
+        code: 'my'
+    },
+    {
+        name: 'Νιγηρία',
+        code: 'ng'
+    },
+    {
+        name: 'Ολλανδία',
+        code: 'nl'
+    },
+    {
+        name: 'Νέα Ζηλανδία',
+        code: 'nz'
+    },
+    {
+        name: 'Φιλιππίνες',
+        code: 'ph'
+    },
+    {
+        name: 'Πολωνία',
+        code: 'pl'
+    },
+    {
+        name: 'Πορτογαλία',
+        code: 'pt'
+    },
+    {
+        name: 'Ρουμανία',
+        code: 'ro'
+    },
+    {
+        name: 'Σερβία',
+        code: 'rs'
+    },
+    {
+        name: 'Ρωσσία',
+        code: 'ru'
+    },
+    {
+        name: 'Νότια Αφρική',
+        code: 'sa'
+    },
+    {
+        name: 'Σουηδία',
+        code: 'se'
+    },
+    {
+        name: 'Σιγκαπούρη',
+        code: 'sg'
+    },
+    {
+        name: 'Σλοβενία',
+        code: 'si'
+    },
+    {
+        name: 'Σλοβακία',
+        code: 'sk'
+    },
+    {
+        name: 'Ταϊλάνδη',
+        code: 'th'
+    },
+    {
+        name: 'Τουρκία',
+        code: 'tr'
+    },
+    {
+        name: 'Ταϊβάν',
+        code: 'tw'
+    },
+    {
+        name: 'Ουκρανία',
+        code: 'ua'
+    },
+    {
+        name: 'Ηνωμένες Πολιτείες',
+        code: 'us'
+    },
+    {
+        name: 'Βενεζουέλα',
+        code: 've'
+    },
+    {
+        name: 'Νότια Αφρική',
+        code: 'za'
+    }
+]
