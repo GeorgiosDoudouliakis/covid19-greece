39b48b5ee7b22c922d22fe7704b50964adefce89
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-11-29 00:13:54 +0200

    refactor: Change layout of pie chart

--- src/app/modules/home/components/gender/gender.component.ts
+++ src/app/modules/home/components/gender/gender.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { GenderService } from '../../services/gender/gender.service';
 import { ChartOptions, ChartType } from 'chart.js';
-import { monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, Label, SingleDataSet } from 'ng2-charts';
+import { monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, SingleDataSet } from 'ng2-charts';
 import { GenderPercentages } from '../../models/gender-percentages.model';
 import { finalize, takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
@@ -14,13 +14,14 @@ import { Subject } from 'rxjs';
 export class GenderComponent implements OnInit, OnDestroy {
   public pieChartOptions: ChartOptions = {
     responsive: true,
+    tooltips: { enabled: false }
   };  
-  public pieChartLabels: Label[] = ['Females', 'Males'];
   public pieChartData: SingleDataSet = [];
   public pieChartType: ChartType = 'pie';
-  public pieChartLegend = true;
-  public pieChartColors: any[] = [{ backgroundColor: ["#FFBF00", "#EC0067"] }];
+  public pieChartColors: any[] = [{ backgroundColor: ["#EC0067", "#333"] }];
   public pieChartPlugins = [];
+  public femalesPercentage: number;
+  public malesPercentage: number;
   private destroy$ = new Subject();
   @Output() isGenderDataLoading = new EventEmitter();
 
@@ -37,6 +38,8 @@ export class GenderComponent implements OnInit, OnDestroy {
     )
     .subscribe((res: GenderPercentages) => {
       const { total_females_percentage, total_males_percentage } = res;
+      this.femalesPercentage = total_females_percentage;
+      this.malesPercentage = total_males_percentage;
       this.pieChartData = [total_females_percentage, total_males_percentage];
     });
   }
