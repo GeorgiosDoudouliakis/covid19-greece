2f86ade338d8f7752587d8c9e8dc20109e29f280
Georgios Doudouliakis <dudugeorge89@gmail.com>
2021-12-02 01:13:27 +0200

    refactor: Delete language selector component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { HeaderComponent } from './components/navigation/header/header.component';
 import { FooterComponent } from './components/navigation/footer/footer.component';
 import { LoaderComponent } from './components/loader/loader.component';
-import { LanguageSelectorComponent } from './components/navigation/header/language-selector/language-selector.component';
 import { ArticleComponent } from './components/article/article.component';
 import { ArrowVisibilityDirective } from './directives/arrow-visibility/arrow-visibility.directive';
 
@@ -23,7 +22,6 @@ import { ArrowVisibilityDirective } from './directives/arrow-visibility/arrow-vi
         FooterComponent, 
         NavigationComponent, 
         LoaderComponent, 
-        LanguageSelectorComponent, 
         ArticleComponent, 
         ArrowVisibilityDirective
     ],
